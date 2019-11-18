fc782fe51753af3c9873ffefb9d1b49d4d504003
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-11-18 14:00:47 +0100

    Commit zéro HelloWorld

---
+++ HelloWorld/Message.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Message
+    {
+        int morningTime;
+        int middayTime;
+        int eveningTime;
+
+        public Message(int morningTime = 9, int middayTime = 13, int eveningTime = 18)
+        {
+            this.morningTime = morningTime;
+            this.middayTime = middayTime;
+            this.eveningTime = eveningTime;
+        }
+
+        public string getHelloMessage()
+        {
+            string me = Environment.UserName;
+            me = me.Replace(".", " ");
+
+            DateTime now = DateTime.Now;
+            int dayOfWeekIndex = (int)now.DayOfWeek;
+
+            if ((dayOfWeekIndex == 1 ? now.Hour >= this.morningTime : dayOfWeekIndex > 0) &&
+                (dayOfWeekIndex == 5 ? now.Hour < this.eveningTime : dayOfWeekIndex < 5))
+                if (now.Hour >= morningTime && now.Hour < this.middayTime)
+                    return $"Bonjour {me}";
+                else if (now.Hour >= this.middayTime && now.Hour < this.eveningTime)
+                    return $"Bon après-midi {me}";
+                else
+                    return $"Bonsoir {me}";
+            else
+                return $"Bon week-end {me}";
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new Message();
+
+            String input;
+            do {
+                Console.WriteLine("Appuyez sur entrée pour votre message, tappez 'exit' pour quitter");
+                input = Console.ReadLine();
+                if (input == "") Console.WriteLine(message.getHelloMessage());
+            } while (input != "exit");
+        }
+    }
+}
